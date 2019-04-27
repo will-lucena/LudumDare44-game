49b1a1f1e419af64d9d328b5540f2f2cfb172ec5
Will <will.lucen4@gmail.com>
2019-04-27 17:23:28 -0300

    Card drag implemented

---
+++ Assets/Scripts/CardSwipe.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CardSwipe : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    private float offsetX;
+    private float offsetY;
+
+    private Vector2 originalPos;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        originalPos.x = transform.position.x;
+        originalPos.y = transform.position.y;
+
+        offsetX = transform.position.x - eventData.position.x;
+        offsetY = transform.position.y - eventData.position.y;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Vector2 newPos = new Vector2(eventData.position.x + offsetX, eventData.position.y + offsetY);
+        transform.position = newPos;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        DragHandler droppedRegion = eventData.pointerCurrentRaycast.gameObject.GetComponent<DragHandler>();
+
+        if (droppedRegion)
+        {
+            droppedRegion.overEvent?.Invoke();
+        }
+        transform.position = originalPos;
+    }
+}
--- Assets/Scripts/DragHandler.cs
+++ Assets/Scripts/DragHandler.cs
@@ -3,19 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class DragHandler : MonoBehaviour, IPointerEnterHandler, IPointerDownHandler
+public class DragHandler : MonoBehaviour
 {
     public System.Action overEvent;
-
-    public void OnPointerDown(PointerEventData eventData)
-    {
-        Debug.Log("down");
-    }
-
-    public void OnPointerEnter(PointerEventData eventData)
-    {
-        Debug.Log("enter");
-        Debug.Log(eventData.pointerPress);
-        //overEvent?.Invoke();
-    }
 }
