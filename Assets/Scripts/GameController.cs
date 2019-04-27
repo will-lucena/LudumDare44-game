33498c8f17aa53b210e18a4d8fcabff0d8d4031d
Will <will.lucen4@gmail.com>
2019-04-27 09:55:05 -0300

    Progress bar logic and scriptable object load done

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public static System.Action<int> endTurn;
+    private static GameProps currentProps;
+
+    private void Awake()
+    {
+        currentProps = Resources.Load<GameProps>("Props/default");
+    }
+
+    public void nextTurn()
+    {
+        endTurn?.Invoke(1);
+    }
+
+    public static int getTotalTurns()
+    {
+        return currentProps.numberOfTurns;
+    }
+}
---
+++ Assets/Scripts/ProgressController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressController : MonoBehaviour
+{
+    [SerializeField] private Slider progressBar;
+
+    private int currentTurn;
+    private int totalTurns;
+
+    private void Start()
+    {
+        GameController.endTurn += updateProgress;
+        this.totalTurns = GameController.getTotalTurns();
+        Debug.Log(totalTurns);
+    }
+
+    public void updateProgress(int amount)
+    {
+        this.currentTurn += amount;
+        this.progressBar.value = ((float) this.currentTurn / (float)this.totalTurns);
+    }
+}
